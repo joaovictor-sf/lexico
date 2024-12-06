134b107d42ca0073999d13f1dbdab055e4145a89
Arthur Xavier <arthur.xavier@ucsal.edu.br>
2024-12-05 21:20:15 -0300

    patch 3.1

--- src/lexico/main/Main3.java
+++ src/lexico/main/Main3.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-
 import lexico.controller.FindFile2;
 import lexico.controller.Lexico;
 import lexico.model.Token;
